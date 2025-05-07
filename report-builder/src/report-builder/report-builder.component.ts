5505fd58f07e9977140d98016db5427847cb46f6
Dharmender <adharmenderkapil@gmail.com>
2025-05-08 00:07:34 +0530

    report Changes Commits

--- report-builder/src/report-builder/report-builder.component.ts
+++ report-builder/src/report-builder/report-builder.component.ts
@@ -139,13 +139,6 @@ availableJobFields: string[] = [
   }
 
   previewReport() {
-    this.reportTitle = ""
-    // Logic for preview
-    // this.rowDataa = [
-    //   { JobID: "Tesla", model: "Model Y", price: 64950, electric: true },
-    //   { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    //   { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-    // ];
 
     this.columnDefs = [
       { field: "JobID", flex: 1 },
